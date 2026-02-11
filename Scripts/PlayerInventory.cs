a05d112b636d1d6951e756fb6f849ccaffb370de
Kirill Garbukov <jonfam71@gmail.com>
2026-02-11 16:11:56 +0300

    PlayerInventory.cs

--- Scripts/PlayerInventory.cs
+++ Scripts/PlayerInventory.cs
@@ -6,13 +6,11 @@ public class PlayerInventory : MonoBehaviour
 {
     private List<int> keys = new List<int>();
 
-    // Проверка, есть ли ключ с таким ID
     public bool HasKey(int id)
     {
         return keys.Contains(id);
     }
 
-    // Добавить ключ
     public void AddKey(int id)
     {
         if (!keys.Contains(id))
